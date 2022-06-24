cbfc6658b01ca96f7d89a1ca6a7d451762e8cc69
emrehliug <guigui410@gmail.com>
2022-06-24 15:38:57 -0300

    incluindo html do component listar-tarefa

--- PraticandoAngular/Multi/src/app/app-routing.module.ts
+++ PraticandoAngular/Multi/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+
 import { CalculadoraComponent } from "./calculadora/components/calculadora.component";
 import { GerenciadorDeTarefasComponent } from "./gerenciador-de-tarefas/components/gerenciador-de-tarefas.component";
+import { TarefasRoutes } from "./gerenciador-de-tarefas/tarefas/tarefas-routing.module";
 
 export const routes: Routes = [
   { path: 'calculadora', component: CalculadoraComponent },
   { path: 'gerenciadordetarefas', component: GerenciadorDeTarefasComponent },
   { path: '', redirectTo: 'calculadora', pathMatch:'full' },
-  { path: '**', redirectTo: 'calculadora', pathMatch:'full' }
+  { path: '**', redirectTo: 'calculadora', pathMatch:'full' },
+  ...TarefasRoutes
 ];
 
 @NgModule
--- PraticandoAngular/Multi/src/app/gerenciador-de-tarefas/gerenciador-de-tarefas.module.ts
+++ PraticandoAngular/Multi/src/app/gerenciador-de-tarefas/gerenciador-de-tarefas.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GerenciadorDeTarefasComponent } from './components/gerenciador-de-tarefas.component';
+import { TarefasModule } from './tarefas/tarefas.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    TarefasModule
   ],
   declarations: [
     GerenciadorDeTarefasComponent
